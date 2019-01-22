37d253cfbb404e70a32ee748c948d2ea7a464ce7
Matheus Henrique <matheus.181137@gmail.com>
2019-01-22 01:28:40 -0300

    base interface done

--- src/manutencao/Interface.java
+++ src/manutencao/Interface.java
@@ -1,4 +1,11 @@
 package manutencao;
 
 public class Interface {
+
+    public void desenhaMenu() {
+        System.out.println("Escolha 1");
+        System.out.println("Escolha 2");
+        System.out.println("Escolha 3");
+        System.out.println("Escolha 4");
+    }
 }
