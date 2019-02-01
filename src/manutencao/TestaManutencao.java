2842e811051a3203ce52b4fef55f2822b202a879
Matheus Henrique <matheus.181137@gmail.com>
2019-01-31 21:02:32 -0300

    trabalhando na classe interface

--- src/manutencao/TestaManutencao.java
+++ src/manutencao/TestaManutencao.java
@@ -1,10 +1,29 @@
 package manutencao;
+import java.util.Scanner;
 
 public class TestaManutencao {
 
     public static void main(String[] args) {
         Interface inter = new Interface();
+        Scanner scan = new Scanner(System.in);
+        int opcao = 0;
+
+
         inter.desenhaMenu();
+        opcao = scan.nextInt();
+
+        if (opcao == 1) {
+            String nome, endereco, telefone, cpf;
+            System.out.println("Digite o nome do cliente");
+            nome = scan.next();
+            System.out.println("Digite o endereco do cliente");
+            endereco = scan.next();
+
+        }
+        else if (opcao == 2)
+            System.out.println("Interface de cadastro de técnicos");
+        else if (opcao == 3)
+            System.out.println("Interface de cadastro de procedimentos");
 
     }
 }
